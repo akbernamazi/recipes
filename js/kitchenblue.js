6a941bfda420b1606863cd33d9707a6b483478c1
akbernamazi <akbarnamazi123@gmail.com>
2020-06-01 10:46:49 +0530

    Update

--- js/kitchenblue.js
+++ js/kitchenblue.js
@@ -106,7 +106,7 @@ function performSearch(event) {
   // Send the request
 
   request = $.ajax({
-      url: "http://www.recipepuppy.com/api/",
+      url: "https://cors-anywhere.herokuapp.com/" + "http://www.recipepuppy.com/api/",
       type: "GET",
       data: { i: $("#ingredients").val(), q: $("#contains").val() }
   });
